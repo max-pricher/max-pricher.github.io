e27dd6a94efffee8f30393ea770c074c79060751
Max <mapr3537@colorado.edu>
2025-11-09 15:34:00 -0700

    refined JS
    refined more JS and fixed the searchTerm block
    fixed index.html

--- Systems-Critique/week-2/system-script.js
+++ Systems-Critique/week-2/system-script.js
@@ -1,157 +1,122 @@
-
-const searchBar = document.getElementById('main-search-bar');
-const updateReminder = document.getElementById('js-update-reminder');
-// empty array for keywords, let allows it to not conflict with other scripts e.g. scope is this file
+// Global arrays
 let keywords = [];
+let blockWords = [];
 
-// listen for all keydowns
-searchBar.addEventListener('keydown', (event) => {
-    // filter only for enter key
-    if (event.key === 'Enter') {
-        // search logic...
+function updateSearchResults() { // searching function
+    let searchResults;
+    if (keywords.includes("_all")) { // if the keywords array contains _all, make a copy of WEBSITES
+        // make a copy of WEBSITES
+        searchResults = [...WEBSITES]; // was originally just = WEBSITES, but JS is pass by reference
+    } else if (keywords.length > 0) { // if not _all and not empty
+        searchResults = []; // get blank array
+        getPossibleResults(searchResults); // populate array
+    }
+    else { // no keyword present
+        // clear results
+        displayResults([]);
+        return;
+    }
+    // apply SEO
+    filterBlockWords(searchResults, blockWords); // Remove blockwords
+    getRelevantResults(searchResults); // Sort by relevance
 
-        // interpret and store keyword
-        let currentKeyword = searchBar.value.trim().toLowerCase();
+    // Later?
+    //applySorting(searchResults);
+    //applyPrefrences(searchResults); 
+
+    // display filtered WEBSITES
+    displayResults(searchResults);
+}
+
+// Search bar logic
+const searchBar = document.getElementById('main-search-bar'); // Get search bar object
+searchBar.addEventListener('keydown', (event) => { // listener for all keydowns, looking for enter key
+    // search logic...
+    if (event.key === 'Enter') { // filter only for enter key
+        let currentKeyword = searchBar.value.trim().toLowerCase(); // interpret and store keyword
         if (currentKeyword) {
-            if (currentKeyword[0] == '-') // treat as block word
+            if (currentKeyword[0] == '-') // leading char is - ||block word
             {
-                // remove the -
-                let blockWord = currentKeyword.substring(1); // remove leading character
-                console.log(`block added: ${blockWord}`);
-                blockWords.push(blockWord);
+                let blockWord = currentKeyword.substring(1); // remove the leading character -
+                blockWords.push(blockWord); // push to blockWord array
             }
-            else // treat as regular keyword
+            else // not special term, treat as regular keyword
             {
-                if (!keywords.includes(currentKeyword)) {
-                    console.log(currentKeyword);
-                    keywords.push(currentKeyword);
+                if (!keywords.includes(currentKeyword)) { // if current keyword isn't already in array
+                    keywords.push(currentKeyword); // add to array
                 }
             } // keyword has been added
-            if (keywords.includes("_all")) {
-                // make a copy of WEBSITES
-                let allWEBSITES = WEBSITES;
-                // remove block words for testing
-                filterblockWords(allWEBSITES);
-                // sort by relevance
-                getRelevantResults(allWEBSITES);
-                // display filtered WEBSITES
-                displayResults(allWEBSITES);
-            } else if (keywords.length > 0) {
-                searchKeywords();
-            }
-            else {
-                // clear results
-                displayResults([]);
-            }
         }
 
-        // clear bar
+        updateSearchResults();
+
+        // clear bar after each keyword input
         searchBar.value = "";
         // display keywords
-        const keywordsContainer = document.getElementById('js-keywords-container');
-        displayKeywordContainer(keywordsContainer);
+        updateKeywordContainer();
     }
 });
 
 
-const searchButton = document.getElementById('js-search-button');
-searchButton.addEventListener('click', () => {
-
-
-    // clear bar when finished
-    searchBar.value = "";
-});
-
-function getPossibleResults(searchResults) {
-    // itterate through every keyword inputted
-    for (let keyword = 0; keyword < keywords.length; keyword++) {
-        // and check every website
-        const currentKeyword = keywords[keyword];
-        for (let website = 0; website < WEBSITES.length; website++) {
-            const currentWebsite = WEBSITES[website];
-            const currentTitle = currentWebsite.title.toLowerCase();
-            const currentDescription = currentWebsite.description.toLowerCase();
-
-            if (currentTitle.includes(currentKeyword) || currentDescription.includes(currentKeyword)) {
-                if (!searchResults.includes(currentWebsite)) {
-                    // push the index of the website to search results
-                    searchResults.push(currentWebsite);
+function getPossibleResults(searchResults) { // Find every result that contains any of the keywords and populate array.
+    for (let keyword = 0; keyword < keywords.length; keyword++) { // itterate through every keyword inputted
+        const currentKeyword = keywords[keyword]; // get current keyword
+        for (let website = 0; website < WEBSITES.length; website++) { // and check every website
+            const currentWebsite = WEBSITES[website]; // get current website
+            if (isMatch(currentKeyword, currentWebsite)) { // if match exists
+                if (!searchResults.includes(currentWebsite)) { // and isnt already in results
+                    searchResults.push(currentWebsite); // add index to results array
                 }
             }
-        }
-    }
+        } // end of website check
+    } // finished searching/end of keyword check
+}
+
+function filterBlockWords(searchResults) { // takes array and filters out any matching results from global blockWords
+    for (let resultsIndex = searchResults.length - 1; resultsIndex >= 0; resultsIndex--) { // itterate backwards to not skip any results when splicing
+        const currentWebsite = searchResults[resultsIndex]; // get current website
+        for (let blockWordsIndex = 0; blockWordsIndex < blockWords.length; blockWordsIndex++) { // and check every block word
+            const currentBlockWord = blockWords[blockWordsIndex];
+
+            if (isMatch(currentBlockWord, currentWebsite)) { // if block word found in site
+                searchResults.splice(resultsIndex, 1); // splice result
+                break; // dont check if any more block words exist
+            }
+        } // end of block words checks
+    } // end of websites check
     // finished searching
 }
 
-let blockWords = [];
-
-function filterblockWords(searchResults) {
-    // itterate through every blockWord in array
-    for (let blockWord = 0; blockWord < blockWords.length; blockWord++) {
-        // and check every result
-        const currentblockWord = blockWords[blockWord];
-        for (let website = 0; website < WEBSITES.length; website++) {
-            const currentWebsite = WEBSITES[website];
-            const currentTitle = currentWebsite.title.toLowerCase();
-            const currentDescription = currentWebsite.description.toLowerCase();
-
-            if (currentTitle.includes(currentblockWord) || currentDescription.includes(currentblockWord)) {
-                // if it exists in the list
-                if (searchResults.includes(currentWebsite)) {
-                    // get index and splice it out, god bless indexOf
-                    const index = searchResults.indexOf(currentWebsite);
-                    searchResults.splice(index, 1);
-                }
-            }
-        }
+function isMatch(currentkeyword, currentWebsite) {
+    const currentTitle = currentWebsite.title.toLowerCase(); // get it's lowercase title
+    const currentDescription = currentWebsite.description.toLowerCase(); // get its lowercase description
+    if (currentTitle.includes(currentkeyword) || currentDescription.includes(currentkeyword)) { // if title or description contains keyword
+        return true;
     }
-    // finished searching
+    return false;
 }
 
 // article used for sorting https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
 
-function getRelevantResults(searchResults) {
-    searchResults.sort((a, b) => {
-        return getKeywordOccurence(b) - getKeywordOccurence(a);
+function getRelevantResults(searchResults) { // sort results by relevance (number of keyword occurences)
+    searchResults.sort((a, b) => { // compare elements a and b, if b is greater, swap
+        return getKeywordOccurence(b) - getKeywordOccurence(a); // greater score wins
     });
 }
 
-// function to return a WEBSITES  keyword occurence score
-function getKeywordOccurence(website) {
+// function to return a WEBSITES keyword occurence score
+function getKeywordOccurence(website) { // how many unique keywords were found in website
     let score = 0;
-    // get title and description
-    const title = website.title.toLowerCase();
-    const description = website.description.toLowerCase();
-
-    // itterate through every keyword
-    for (let keyword = 0; keyword < keywords.length; keyword++) {
+    for (let keyword = 0; keyword < keywords.length; keyword++) { // itterate through every keyword
         const currentKeyword = keywords[keyword];
-        if (title.includes(currentKeyword) || description.includes(currentKeyword)) {
+        if (isMatch(currentKeyword, website)) { // if website contains at least one occurence of keyword
             score++;
         }
     }
-    return score;
+    return score; // how many unique keywords were found
 }
-
-function searchKeywords() {
-    let searchResults = [];
-    // pass by reference is default in JS
-    getPossibleResults(searchResults);
-    filterblockWords(searchResults);
-    getRelevantResults(searchResults);
-    //filterBlocklist(searchResults); 
-    //applySorting(searchResults);
-    //applyPrefrences(searchResults); 
-
-
-    //done searching
-    console.log(searchResults.length);
-    displayResults(searchResults);
-}
-
 
 // display results
-
 function displayResults(searchResults) {
     // get container
     const resultsContainer = document.getElementById('js-search-container');
@@ -165,18 +130,16 @@ function displayResults(searchResults) {
     resultsContainer.innerHTML = ""; // reset
     // create every website
 
-    for (let i = 0; i < searchResults.length; i++) {
-        const website = searchResults[i];
+    for (let i = 0; i < searchResults.length; i++) { // load ALL results
+        const website = searchResults[i]; // get current website
         // create div
         const card = document.createElement('div');
-        // add class
-        card.className = 'result-card';
+        card.className = 'result-card'; // add class
 
-        // create href
+        // setup href
         const websiteLink = document.createElement('a');
-        websiteLink.href = website.url;
-        // tells browser to open in new tab
-        websiteLink.target = '_blank';
+        websiteLink.href = website.url; // assign URL
+        websiteLink.target = '_blank'; // tells browser to open in new tab
 
         // create title
         const websiteTitle = document.createElement('h3');
@@ -184,68 +147,58 @@ function displayResults(searchResults) {
 
         // create description
         const websiteDescription = document.createElement('p');
-        websiteDescription.textContent = website.description;
+        websiteDescription.textContent = website.description; // assign description
 
-        // append to card
-        websiteLink.appendChild(websiteTitle);
-        card.appendChild(websiteLink);
-        card.appendChild(websiteDescription);
-
-        // add finished container to results
-        resultsContainer.appendChild(card);
+        // append elements to card
+        websiteLink.appendChild(websiteTitle); // append h3 title
+        card.appendChild(websiteLink); // append link 
+        card.appendChild(websiteDescription); // append description
+        resultsContainer.appendChild(card); // add finished container to results
     }
 }
 
 
-// display keywords in containers
+// Display keywords in containers
+function updateKeywordContainer() {
+    const keywordsContainer = document.getElementById('js-keywords-container'); // get keyword container
+    keywordsContainer.innerHTML = ""; // clear render
 
+    const keywordsLength = keywords.length;
 
+    for (let block = 0; block < keywordsLength + blockWords.length; block++) { // add all keywords
+        const searchTerm = document.createElement('button'); // create button
+        searchTerm.className = 'searchTerm-block'; // add class to button
 
-function displayKeywordContainer(keywordsContainer) {
-    // clear render
-    keywordsContainer.innerHTML = "";
-    // add keywords
-    for (let keyword = 0; keyword < keywords.length; keyword++) {
-        // button because we  want our element to be able to focus and be clickable
-        const keywordBlock = document.createElement('button');
-        keywordBlock.className = 'keyword-block';
-        keywordBlock.textContent = keywords[keyword];
+        // populate with data
+        if (block < keywords.length) { // if were searching keywords
+            searchTerm.classList.add('keyword-block'); // add class to button
+            searchTerm.textContent = keywords[block]; // add content to button
+        }
+        else { // add block words
+            let blockWordIndex = block - keywordsLength;
+            searchTerm.classList.add('blockWord-block'); // add class to button
+            searchTerm.textContent = blockWords[blockWordIndex];
+        }
 
-        // needs to be within function due to it being dynamic
-        keywordBlock.addEventListener('click', () => {
-
-            // if leading char is - treat as block word
-            if (keyword[0] == '-') {
-                // treat the keyword as a block word
-                blockWords.splice(keyword, 1);
+        // search term removal handler
+        searchTerm.addEventListener('click', () => { // add listener to element
+            // get text to remove since button doesnt know it's own index just it's content
+            const buttonText = searchTerm.textContent;
+            if (searchTerm.className == "keyword-block") { // if class is a keyword block
+                const index = keywords.indexOf(buttonText);
+                if (index > -1) {
+                    keywords.splice(index, 1); // remove the block from the keywords array
+                }
             }
-            // if keyword exists or in bounds
-            else if (keyword > -1) {
-                // splice one element from array and indice of keyword 
-                keywords.splice(keyword, 1);
-            }
-            const keywordsContainer = document.getElementById('js-keywords-container');
-
-            // clear old render
-            keywordsContainer.innerHTML = "";
-            displayKeywordContainer(keywordsContainer);
-        }); // end of event listener
-        keywordsContainer.appendChild(keywordBlock);
-    }
-    for (let blockWord = 0; blockWord < blockWords.length; blockWord++) {
-        const blockWordBlock = document.createElement('button');
-        blockWordBlock.className = 'blockWord-block';
-        blockWordBlock.textContent = blockWords[blockWord];
-        // needs to be within function due to it being dynamic
-        // when clicked, removes block word from array
-        blockWordBlock.addEventListener('click', () => {
-            // if block word exists or in bounds
-            if (blockWord > -1) {
-                // splice one element from array and indice of block word 
-                blockWords.splice(blockWord, 1);
-            }
-            displayKeywordContainer(keywordsContainer);
+            else { // block word
+                const index = blockWords.indexOf(buttonText);
+                if (index > -1) {
+                    blockWords.splice(index, 1); // remove the block from the blockWords array
+                }
+            } // keyword has been removed
+            updateSearchResults(); // update search results
+            updateKeywordContainer(); // re-render keywords
         });
-        keywordsContainer.appendChild(blockWordBlock);
+        keywordsContainer.appendChild(searchTerm); // add finished child
     }
 }
